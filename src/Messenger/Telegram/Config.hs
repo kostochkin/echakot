77bb22e631ab8e88ce97781851251b63a9b66e91
Konstantin Gorshkov <k.n.gorshkov@gmail.com>
2020-01-22 18:58:02 +0300

    little refactoring

--- src/Messenger/Telegram/Config.hs
+++ src/Messenger/Telegram/Config.hs
@@ -1,30 +1,34 @@
 module Messenger.Telegram.Config
-    ( defaultTelegramConfig, botToken, pollingTimeout, timeout, messagesLimit, limit
+    ( defaultTelegramConfig
+    , botToken
+    , pollingTimeout
+    , timeout
+    , updatesLimit
+    , limit
     ) where
 
 
 data PollingTimeout = LongPolling Integer | ShortPolling deriving Show
 
-newtype Limit = Limit Integer deriving Show
+newtype Limit = Limit {getLimit :: Integer} deriving Show
+
+data TelegramConfig = TelegramConfig
+    { url :: String
+    , botToken :: String
+    , pollingTimeout :: PollingTimeout
+    , updatesLimit :: Limit
+    } deriving Show
+
+defaultTelegramConfig = TelegramConfig
+    { url = "https://api.telegram.org"
+    , botToken = ""
+    , pollingTimeout = ShortPolling
+    , updatesLimit = limit 100
+    }
 
 timeout x
    | x > 0 = LongPolling x
    | otherwise = ShortPolling
 
 limit = Limit . max 1 . min 100
-
-data TelegramConfig = TelegramConfig
-    { url :: String,
-      botToken :: String,
-      pollingTimeout :: PollingTimeout,
-      messagesLimit :: Limit
-      
-    } deriving Show
-
-defaultTelegramConfig = TelegramConfig
-    { url = "https://api.telegram.org",
-      botToken = "",
-      pollingTimeout = ShortPolling,
-      messagesLimit = limit 100
-    }
 
