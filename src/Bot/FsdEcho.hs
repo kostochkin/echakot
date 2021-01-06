9a9ad96760d78eac604466ec0674f0e577fcc014
Konstantin Gorshkov <k.n.gorshkov@gmail.com>
2021-01-07 00:29:34 +0300

    generalized bot type

--- src/Bot/FsdEcho.hs
+++ src/Bot/FsdEcho.hs
@@ -3,19 +3,19 @@ module Bot.FsdEcho (botStep) where
 import Bot.Api
 import Control.Monad
 
-botStep :: BotApi String Int ()
+botStep :: BotApi b Int ()
 botStep = do
     ms <- getMessages
     mapM_ procMessage ms
     return ()
 
-procMessage :: String -> BotApi String Int ()
+procMessage :: b -> BotApi b Int ()
 procMessage m = do
     a <- selectAction m
     botAction a m
 
 
-botAction :: Action Int -> String -> BotApi String Int ()
+botAction :: Action Int -> b -> BotApi b Int ()
 botAction Help             = const $ showHelp
 botAction TellRepeat       = const $ tellCurrentRepeats >> showKeyboard
 botAction (ModifyRepeat i) = const $ setRepeats i
